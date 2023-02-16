447b31eae4cf4e17151fc13783b71a5eeafe575c
wambug <wambugumacharia35@gmail.com>
2023-02-16 21:04:33 +0300

    error

--- assembler/src/error.rs
+++ assembler/src/error.rs
@@ -2,10 +2,6 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum CompError {
-    #[error("Invalid header (expected {expected:?}, got {found:?})")]
-    InvalidHeader { expected: String, found: String },
-    #[error("Missing attribute: {0}")]
-    MissingAttribute(String),
     #[error("Size not Implemented")]
     InvalidSize,
     #[error("Instruction not found")]
